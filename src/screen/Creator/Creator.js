a6e3fde723dd1dba7962b0a0b85e72e42407ff6d
oguz yurken <oguz.yurken@gmail.com>
2022-11-10 22:27:11 +0300

    Update Creator

--- src/screen/Creator/Creator.js
+++ src/screen/Creator/Creator.js
@@ -1,10 +1,53 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image, Linking, TouchableOpacity} from 'react-native';
 import React from 'react';
+import style from './Creator.style';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import Hello from '../../components/Hello/Hello';
+
+const openGithub = () => {
+  Linking.openURL('https://github.com/yurkenn');
+};
+
+const openLinkedin = () => {
+  Linking.openURL('https://www.linkedin.com/in/oğuzyürken');
+};
+
+const openTwitter = () => {
+  Linking.openURL('https://twitter.com/yurkenowl');
+};
 
 const Creator = () => {
   return (
-    <View>
-      <Text>Creator</Text>
+    <View style={style.container}>
+      <View style={style.image_container}>
+        <Image
+          style={style.image}
+          source={{uri: 'https://avatars.githubusercontent.com/u/69719126?v=4'}}
+        />
+      </View>
+      <Hello style={style.hello_animation} />
+
+      <View style={style.about_container}>
+        <Text style={style.name}>Oğuz Yürken</Text>
+        <Text style={style.nickname}>yurkenn</Text>
+        <Text style={style.decription}>Mobile Developer | React Native</Text>
+      </View>
+
+      {/* my links  */}
+      <View style={style.links}>
+        <TouchableOpacity style={style.github} onPress={openGithub}>
+          <Icon name="github" size={35} color="#000" />
+          <Text style={style.gitText}>Github</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={style.linkedin} onPress={openLinkedin}>
+          <Icon name="linkedin" size={35} color="#000" />
+          <Text style={style.linkText}>Linkedin</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={style.twitter} onPress={openTwitter}>
+          <Icon name="twitter" size={35} color="#000" />
+          <Text style={style.twitterText}>Twitter</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
--- src/screen/Creator/Creator.style.js
+++ src/screen/Creator/Creator.style.js
@@ -1,3 +1,76 @@
-import {StyleSheet} from 'react-native';
+import {Dimensions, StyleSheet} from 'react-native';
+const getWidth = Dimensions.get('window').width;
+const getHeight = Dimensions.get('window').height;
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  image_container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: getHeight * 0.02,
+  },
+  image: {
+    width: getWidth / 2,
+    height: getHeight / 4,
+  },
+  about_container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: getHeight * 0.05,
+  },
+  name: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  nickname: {
+    textAlign: 'center',
+    color: '#000',
+    fontSize: 15,
+  },
+  decription: {
+    fontSize: 15,
+    color: '#000',
+    fontStyle: 'italic',
+    marginTop: getHeight * 0.01,
+  },
+  hello_animation: {
+    position: 'absolute',
+    top: getHeight * 0.1,
+    left: getWidth * 0.13,
+    right: getWidth * 0.2,
+    bottom: getHeight * 0.2,
+    width: getWidth / 2,
+    height: getHeight / 5,
+  },
+
+  links: {
+    marginTop: getHeight * 0.02,
+  },
+  github: {
+    margin: getHeight * 0.01,
+    alignItems: 'center',
+  },
+  gitText: {
+    color: '#000',
+  },
+  linkedin: {
+    margin: getHeight * 0.01,
+    alignItems: 'center',
+  },
+  linkText: {
+    color: '#000',
+  },
+  twitter: {
+    margin: getHeight * 0.01,
+    alignItems: 'center',
+  },
+  twitterText: {
+    color: '#000',
+  },
+});
