d41165e7a28d75887069fb531b8d16fe6351c9a9
oguz yurken <oguz.yurken@gmail.com>
2022-11-11 04:01:04 +0300

    Update Screen and Add Redux pack

--- App.js
+++ App.js
@@ -3,60 +3,64 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import Home from './src/screen/Home';
-import About from './src/screen/About';
+import Favorites from './src/screen/Favorites';
 import Creator from './src/screen/Creator';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import store from './src/redux/store';
+import {Provider} from 'react-redux';
 const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          tabBarActiveTintColor: '#e91e63',
-          tabBarInactiveTintColor: '#000',
-          tabBarStyle: {
-            backgroundColor: '#fff',
-            borderTopColor: 'transparent',
-            elevation: 0,
-          },
-          headerTitleAlign: 'center',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-            color: '#000',
-          },
-        }}>
-        <Tab.Screen
-          name="Home"
-          component={Home}
-          options={{
-            tabBarIcon: ({color, size}) => (
-              <Icon name="home" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="About"
-          component={About}
-          options={{
-            tabBarIcon: ({color, size}) => (
-              <Icon name="info" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Creator"
-          component={Creator}
-          options={{
-            tabBarIcon: ({color, size}) => (
-              <Icon name="person" color={color} size={size} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={{
+            tabBarActiveTintColor: '#e91e63',
+            tabBarInactiveTintColor: '#000',
+            tabBarStyle: {
+              backgroundColor: '#fff',
+              borderTopColor: 'transparent',
+              elevation: 0,
+            },
+            headerTitleAlign: 'center',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              color: '#000',
+            },
+          }}>
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+              tabBarIcon: ({color, size}) => (
+                <Icon name="home" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Favorites"
+            component={Favorites}
+            options={{
+              tabBarIcon: ({color, size}) => (
+                <Icon name="info" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Creator"
+            component={Creator}
+            options={{
+              tabBarIcon: ({color, size}) => (
+                <Icon name="person" color={color} size={size} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
