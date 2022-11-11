fbd4d9f2b4667f0e86155a62a7d9d084844fd22b
oguz yurken <oguz.yurken@gmail.com>
2022-11-12 00:33:19 +0300

    Add Delete button with redux

--- src/components/Cards/FavoriteCard/FavoriteCard.js
+++ src/components/Cards/FavoriteCard/FavoriteCard.js
@@ -1,21 +1,37 @@
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, Pressable} from 'react-native';
 import React from 'react';
 import style from './FavoriteCard.style';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {useDispatch} from 'react-redux';
+import {removeFavorite} from '../../../redux/favoriteSlice';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
 const FavoriteCard = ({favorite}) => {
+  // dispatch
+  const dispatch = useDispatch();
+
+  // handle remove selected Favorite
+  const handleRemoveFavorite = () => {
+    return dispatch(removeFavorite(favorite, favorite.title));
+  };
+
+  //onblur
+
   return (
-    <View style={style.container}>
-      <Text style={style.title}>{favorite.title}</Text>
-      <Image style={style.image} source={{uri: favorite.link}} />
-      <View style={style.subname_container}>
-        <Text style={style.subname}>{favorite.subreddit_name}</Text>
-        <Icon name="face" size={20} color="#000" />
+    <TouchableOpacity onLongPress={handleRemoveFavorite}>
+      <View style={style.container}>
+        <Text style={style.title}>{favorite.title}</Text>
+        <Image style={style.image} source={{uri: favorite.link}} />
+        <View style={style.subname_container}>
+          <Text style={style.subname}>{favorite.subreddit_name}</Text>
+          <Icon name="face" size={20} color="#000" />
+        </View>
+        <View style={style.vote_container}>
+          <Icon name="thumb-up" size={20} color="#000" />
+          <Text style={style.upvote}>{favorite.up_votes}</Text>
+        </View>
       </View>
-      <View style={style.vote_container}>
-        <Icon name="thumb-up" size={20} color="#000" />
-        <Text style={style.upvote}>{favorite.up_votes}</Text>
-      </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
