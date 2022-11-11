b547ec8e513a6ed1bb036dc4eb8ea89e61ee6912
oguz yurken <oguz.yurken@gmail.com>
2022-11-12 00:35:57 +0300

    Update Style

--- src/screen/Favorites/Favorites.style.js
+++ src/screen/Favorites/Favorites.style.js
@@ -1,3 +1,20 @@
-import {StyleSheet} from 'react-native';
+import {Dimensions, StyleSheet} from 'react-native';
+const getWidth = Dimensions.get('window').width;
+const getHeight = Dimensions.get('window').height;
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Empty: {
+    width: getWidth * 0.5,
+    height: getHeight * 0.5,
+  },
+
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
