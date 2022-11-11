91e2499524c370b7becd5ceab73625b7a1299108
oguz yurken <oguz.yurken@gmail.com>
2022-11-12 00:33:47 +0300

    Add Animated

--- src/components/Cards/HomeCard/HomeCard.js
+++ src/components/Cards/HomeCard/HomeCard.js
@@ -1,53 +1,63 @@
-import {View, Text, Image, Pressable} from 'react-native';
-import React, {useState} from 'react';
+import {View, Text, Image} from 'react-native';
+import React, {useCallback, useRef, useState} from 'react';
 import style from './HomeCard.style';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {useDispatch} from 'react-redux';
 import {addFavorite} from '../../../redux/favoriteSlice';
-import Heart from '../../Heart/Heart';
+import {TapGestureHandler} from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withSpring,
+} from 'react-native-reanimated';
+
+const AnimatedImage = Animated.createAnimatedComponent(Image);
 
 const HomeCard = ({memes, onPress}) => {
+  // dispatch
   const dispatch = useDispatch();
 
-  const [isFavorite, setIsFavorite] = useState(false);
+  // animated
+  const doubleTabRef = useRef();
+  const scale = useSharedValue(0);
+  const rStlye = useAnimatedStyle(() => ({
+    transform: [{scale: scale.value}],
+  }));
 
-  const doubleTap = () => {
-    let tapCount = 0;
-    let timer = null;
-    return () => {
-      tapCount++;
-      if (tapCount === 1) {
-        timer = setTimeout(() => {
-          //single tap
-          onPress();
-          tapCount = 0;
-        }, 500);
-      } else if (tapCount === 2) {
-        clearTimeout(timer);
-        //double tap
-        console.log('double tap');
-        tapCount = 0;
-        setIsFavorite(!isFavorite);
-        dispatch(addFavorite(memes));
+  const onDoubleTap = useCallback(() => {
+    scale.value = withSpring(1, undefined, isFinished => {
+      if (isFinished) {
+        scale.value = withDelay(400, withSpring(0));
       }
-    };
-  };
+    });
+    return dispatch(addFavorite(memes));
+  }, [scale, dispatch, memes]);
 
   return (
-    <Pressable onPress={doubleTap()}>
-      <View style={style.container}>
-        <Text style={style.title}>{memes.title}</Text>
-        <Image style={style.image} source={{uri: memes.link}} />
-        <View style={style.subname_container}>
-          <Text style={style.subname}>{memes.subreddit_name}</Text>
-          <Icon name="face" size={20} color="#000" />
+    <TapGestureHandler
+      ref={doubleTabRef}
+      numberOfTaps={2}
+      onActivated={onDoubleTap}>
+      <Animated.View>
+        <View style={style.container}>
+          <Text style={style.title}>{memes.title}</Text>
+          <Image style={style.image} source={{uri: memes.link}} />
+          <AnimatedImage
+            source={require('../../../assets/like.png')}
+            style={[style.heart, rStlye]}
+          />
+          <View style={style.subname_container}>
+            <Text style={style.subname}>{memes.subreddit_name}</Text>
+            <Icon name="face" size={20} color="#000" />
+          </View>
+          <View style={style.vote_container}>
+            <Icon name="thumb-up" size={20} color="#000" />
+            <Text style={style.upvote}>{memes.up_votes}</Text>
+          </View>
         </View>
-        <View style={style.vote_container}>
-          <Icon name="thumb-up" size={20} color="#000" />
-          <Text style={style.upvote}>{memes.up_votes}</Text>
-        </View>
-      </View>
-    </Pressable>
+      </Animated.View>
+    </TapGestureHandler>
   );
 };
 
