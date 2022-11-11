068b80a57336cf5c38dc49fad1e49695643dabaa
oguz yurken <oguz.yurken@gmail.com>
2022-11-12 00:36:38 +0300

    Update Animation file direction and Add keyExtrac

--- src/screen/Home/Home.js
+++ src/screen/Home/Home.js
@@ -1,11 +1,11 @@
-import {FlatList} from 'react-native';
+import {FlatList, View} from 'react-native';
 import React, {useState} from 'react';
 import useApi from '../../hooks/useApi';
 import Config from 'react-native-config';
 import HomeCard from '../../components/Cards/HomeCard';
-import Loading from '../../components/Loading/Loading';
-import Error from '../../components/Error/Error';
-
+import Loading from '../../components/Lottie/Loading/Loading';
+import Error from '../../components/Lottie/Error/Error';
+import style from './Home.style';
 const Home = () => {
   const [data, loading, error, fetchData] = useApi(Config.API_URL);
   const [isRefresing, setIsRefresing] = useState(false);
@@ -22,17 +22,22 @@ const Home = () => {
     return <Error />;
   }
 
+  let keyExtractor = (item, index) => index.toString();
+
   return (
-    <FlatList
-      data={data}
-      renderItem={renderHomeCard}
-      refreshing={isRefresing}
-      onRefresh={() => {
-        setIsRefresing(true);
-        fetchData();
-        setIsRefresing(false);
-      }}
-    />
+    <View style={style.container}>
+      <FlatList
+        data={data}
+        renderItem={renderHomeCard}
+        keyExtractor={keyExtractor}
+        refreshing={isRefresing}
+        onRefresh={() => {
+          setIsRefresing(true);
+          fetchData();
+          setIsRefresing(false);
+        }}
+      />
+    </View>
   );
 };
 
