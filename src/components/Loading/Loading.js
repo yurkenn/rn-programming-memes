a9b987ad3789cc113098941e7483c0f41da43b02
oguz yurken <oguz.yurken@gmail.com>
2022-11-12 00:31:58 +0300

    Change File direction

--- src/components/Loading/Loading.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Lottie from 'lottie-react-native';
-const Loading = () => {
-  return <Lottie source={require('../../assets/loading.json')} autoPlay loop />;
-};
-
-export default Loading;
