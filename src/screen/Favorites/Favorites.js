a967364807a2be8de6f50bd5919476459eecc10d
oguz yurken <oguz.yurken@gmail.com>
2022-11-12 00:35:42 +0300

    Add Empty screen and Empty animation

--- src/screen/Favorites/Favorites.js
+++ src/screen/Favorites/Favorites.js
@@ -2,15 +2,39 @@ import {View, Text, FlatList} from 'react-native';
 import React from 'react';
 import {useSelector} from 'react-redux';
 import FavoriteCard from '../../components/Cards/FavoriteCard';
-
+import Empty from '../../components/Lottie/Empty/Empty';
+import style from './Favorites.style';
 const Favorites = () => {
   const favoriteMemes = useSelector(state => state.favorite.value);
 
   const renderFavorite = ({item}) => {
-    return <FavoriteCard favorite={item} />;
+    return (
+      <View style={style.container}>
+        <FavoriteCard favorite={item} />
+      </View>
+    );
   };
 
-  return <FlatList data={favoriteMemes} renderItem={renderFavorite} />;
+  let keyExtractor = (item, index) => index.toString();
+
+  // no favorites you need to add favorites
+  if (favoriteMemes.length === 0) {
+    return (
+      <View>
+        <Empty style={style.Empty} />
+        <Text style={style.text}>No favorites yet</Text>
+      </View>
+    );
+  }
+  console.log('favoriteMemes', favoriteMemes);
+  return (
+    <FlatList
+      data={favoriteMemes}
+      renderItem={renderFavorite}
+      keyExtractor={keyExtractor}
+      horizontal
+    />
+  );
 };
 
 export default Favorites;
